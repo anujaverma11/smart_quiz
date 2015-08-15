71073d27059e76a94c581d116124e1329ae51078
Robert Baker <robertbaker13@gmail.com>
2015-08-15 15:19:31 -0700

    add gem requirements for httparty, oauth, and dotenv

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
 require 'rubygems'
+require 'dotenv'
+Dotenv.load
 
 require 'uri'
 require 'pathname'
@@ -19,6 +21,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
+require 'httparty'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
